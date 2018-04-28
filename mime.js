fff20390688df65b29b17ee0d5b73e74cd8b4fc8
YunxiaoXu <xuyunxiao2001@gmail.com>
2018-04-28 19:09:19 +0800

    enable Upper Case extname

--- mime.js
+++ mime.js
@@ -4,6 +4,7 @@ const mimeTypes = require('./config/mime.json');
 const lookup  = (pathName) => {
 	let ext = path.extname(pathName);
 	ext = ext.split('.').pop();
+	ext = ext.toLowerCase();
 	return mimeTypes[ext] || mimeTypes['txt'];
 }
 
